a90e0bbb3d67a0c839e1be3e96c5c62bf8c37225
Katsarova <petya.katsarova@cgi.com>
2024-01-23 09:21:39 +0100

    work in progress

---
+++ day24/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+)
+
+// 0.1: split row and calc future moves
+
+// 1. check individually 2 lines if future path crossses
+
+// 2. rotate between all lines and perform above func to see if paths intersect, return
+// the count of how many will intersect
+
+func main() {
+	fmt.Println("lala day 24")
+}
